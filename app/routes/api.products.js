2238e2c3189b1d88d950e2dc1de0ca8e0da0bc1b
pushpakgupta <pushpak.gupta2@gmail.com>
2024-08-31 22:55:00 +0530

    backend  setup new

--- app/routes/api.products.js
+++ app/routes/api.products.js
@@ -1,28 +1,74 @@
-import { authenticate } from "../shopify.server";
+
+import { json} from '@remix-run/node'; 
+import { runBulkOperation } from './utills/BulkOprations/products';
+import fetch from 'node-fetch';
 
 
 
-export const loader = async ({request}) =>{
+export const loader= async ({request}) => {   
+  console.log("starting....") 
+        // const { shop, accessToken } = await authenticate.admin(request);
+        // console.log(shop, accessToken ,  "loader run");
+    const url = new URL(request.url)
+const shop = url.searchParams.get('shop');
+const accessToken = request.headers.get("X-Shopify-Access-Token");
+console.log(shop , accessToken );
+    // try {
+    //   const data = await runBulkOperation(shop , accessToken );
+    //   console.log(data ,  "api retun ")
+    //   return json(data);
+    // } catch (error) {
+    //   return json({ error: error.message }, { status: 500 });
+    // }
 
-    const { shop, accessToken } = await authenticate.admin(request);
+//     const query = `
+//     mutation {
+//       bulkOperationRunQuery(
+//         query: """
+//           {
+//             products {
+//               edges {
+//                 node {
+//                   id
+//                   title
+//                 }
+//               }
+//             }
+//           }
+//         """
+//       ) {
+//         bulkOperation {
+//           id
+//           status
+//         }
+//         userErrors {
+//           field
+//           message
+//         }
+//       }
+//     }
+//   `;
 
-    const response = await fetch(`https://${shop}/admin/api/2024-04/products.json` , {
-        method: "GET",
-        headers:{
-            "X-Shopify-Access-Token" : accessToken,
-            "Content-Type": "application/json"
-        }
-    });
+//   try {
+//     const response = await fetch(`https://${shop}/admin/api/2024-07/graphql.json`, {
+//       method: 'POST',
+//       headers: {
+//         'Content-Type': 'application/json',
+//         'X-Shopify-Access-Token': accessToken,
+//       },
+//       body: JSON.stringify({ query }),
+//     });
+// console.log(response , "response");
+//     if (!response.ok) {
+//       const errorData = await response.json();
+//       throw new Error(`Shopify API error: ${response.statusText} - ${JSON.stringify(errorData)}`);
+//     }
 
-    if(!response.ok){
-        return json({
-            error:"Failed to fetch products",
-            status: response.status
-        })
-    }
+//     const data = await response.json();
+//     return data;
+//   } catch (error) {
+//     console.error('Error during Shopify API request:', error);
+//     throw error;
+//   }
 
-    const products = await response.json()
-
-    return json(products);
-
-}
+  };
