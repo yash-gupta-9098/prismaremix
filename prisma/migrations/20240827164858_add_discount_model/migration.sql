a27bd1471662b687a4b2247e35ec8d752d313b7f
pushpakgupta <pushpak.gupta2@gmail.com>
2024-10-08 23:08:28 +0530

    connected to mongoDB

--- prisma/migrations/20240827164858_add_discount_model/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "Discount" (
-    "id" TEXT NOT NULL PRIMARY KEY,
-    "title" TEXT NOT NULL,
-    "startsAt" DATETIME,
-    "endsAt" DATETIME,
-    "minimumRequirementSubtotal" REAL,
-    "discountAmount" REAL
-);
